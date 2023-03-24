3c526fbbf59264b72c9190ab897fea7a727102bd
Azeve-do <caiopaznunes@gmail.com>
2023-03-23 21:18:42 -0300

    n funcionou php

--- dados.php
+++ dados.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
+    $nome_pessoa = $_GET['inome'];
+    $peso_apurado = $_GET['ipeso'];
+    $nome_ubs = $_GET['iubs'];
+    $semana = $_GET['isemana'];
+
+
+/* Conexão com o banco de dados */
+
+    $conectar_dados = mysqli_connect('localhost', 'root', '', 'maxlav peso');
     
-</body>
-</html>
+    if($conectar_dados -> connect_error){
+        die('Connection failed: '.$conectar_dados -> connect_error);
+    }
+    else{
+        $db = $conectar_dados -> prepare("insert into quilos_semanais (Nome Profissional, Peso, Semana, Unidade) values(?,?,?,?");
+        $db -> bind_param("ssss", $nome_pessoa, $peso_apurado, $nome_ubs, $semana);
+        $db -> execute(); 
+        echo "Registro criado na tabela.";
+        $db -> close();
+        $conectar_dados -> close();
+    }
+?>
